9102b963742820626e89952d2b187466c78f8b26
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-22 09:20:27 +0530

    Add files via upload

---
+++ Day2/song.java
@@ -0,0 +1,19 @@
+class songs
+{
+    String s3="Vanakkum chennai";
+    public void s3_rec()
+    {
+        System.out.println(s3);
+    }
+}
+
+
+public class song
+{
+    public static void main(String ads[])
+    {
+        songs obj=new songs();
+        obj.s3_rec();
+
+    }
+}
