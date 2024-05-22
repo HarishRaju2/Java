3db1e22286a38fa215c6cdde7823bacf41a74084
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-22 09:43:15 +0530

    Add files via upload

---
+++ Day10/lamba.java
@@ -0,0 +1,19 @@
+/**
+ * sample
+ */
+interface sample {
+    public void data();
+
+    
+}
+
+public class lamba {
+    public static void main(String[] args) {
+        sample obj=()->{
+            System.out.println("Harish");
+
+        };
+        obj.data();
+    }
+    
+}
