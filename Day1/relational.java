cea679a3d59e645b2b7b7889bf8c1d5ac046eda2
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-22 09:16:05 +0530

    Create relational.java

---
+++ Day1/relational.java
@@ -0,0 +1,30 @@
+class leap
+{
+    public main leap()
+    {
+        int yr=2003;
+        if(yr%4==0)
+        {
+            if(yr%100!=0)
+            {
+                System.out.print("leap year");
+            }
+            else
+            {
+                System.out.print(" notleap year");
+            }
+        }
+        else{
+            System.out.print(" not leap year");
+        }
+    }
+}
+
+public class relational
+{
+    public static void main(String args[])
+    {
+        leap obj=new obj();
+
+    }
+}
