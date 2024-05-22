3b79cdac6b2ecf9c0ce1e20bef38ba6d1533943a
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-22 09:40:16 +0530

    Create mobile_pack.java

---
+++ Day9/mobile_pack.java
@@ -0,0 +1,16 @@
+import packages_java.subpackages.sub;
+import packages_java.mobile;
+public class mobile_pack
+{
+    public static void main(String[] ads)
+    {
+        sample obj=new sample();
+
+    }
+}
+class sample extends mobile{
+    sample()
+    {
+        super.display();
+    }
+}
